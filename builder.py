52537ad48f1d08fe3686e850160300bedeaa42d1
Son-Guhun <22954418+Son-Guhun@users.noreply.github.com>
2019-05-02 08:37:44 -0300

    Improvements to builderlib code and doc
    - Added more global variables (LNI and SLK versions of DEVELOPMENT and RELEASE).
    - Added test_map function.
    - Now uses a try/except block when attempting to read the configuration file.

--- builder.py
+++ builder.py
@@ -1,30 +1,32 @@
 import configparser
-
-p = configparser.ConfigParser()
-
-p.read('config.ini')
 import subprocess
 import os
+import shutil
+import webbrowser
+import builderlib
+
+try:
+    p = configparser.ConfigParser()
+    p.read('config.ini')
+except:
+    print("Unable to load configuration file.")
 
 
-
-
-def test(version='',build='development'):
-    subprocess.Popen([p['paths']['war3'],'-loadfile',os.path.abspath(build+'{}.w3x'.format('_' + version if version else ''))])
-
-
+DEVELOPMEN_LNI = 'development'
+RELEASE_LNI = 'release'
 DEVELOPMENT = 'development.w3x'
 RELEASE = 'release.w3x'
+DEVELOPMENT_SLK = 'development_slk.w3x'
+RELEASE_SLK = 'release_slk.w3x'
 
-def open_with_editor(file=DEVELOPMENT):
-    subprocess.Popen([p['paths']['worldedit'],'-loadfile',os.path.abspath(file)])
 
-config_values = {
+config_values = \
+{
     'paths': {'w2l':'',
-              'war3':'',
-              'worldedit': ''},
-    'jass-preprocessors': { # Accepts any number of programs, and executes them in order
-
+            'war3':'',
+            'worldedit': ''},
+    # Accept any number of programs, and executes them in order
+    'jass-preprocessors': { 
     },
     'obj-postprocessors': {
 
@@ -32,7 +34,18 @@ config_values = {
     'slk-postprocessors': {
 
     },
-    }
+}
+
+def test(version='',build='development'):
+    """Opens a .w3x map with Warcraft III."""
+    subprocess.Popen([p['paths']['war3'],'-loadfile',os.path.abspath(build+'{}.w3x'.format('_' + version if version else ''))])
+    
+def test_map(map=DEVELOPMENT_SLK):
+    subprocess.Popen([p['paths']['war3'],'-loadfile',os.path.abspath(map)])
+
+def open_with_editor(file=DEVELOPMENT):
+    subprocess.Popen([p['paths']['worldedit'],'-loadfile',os.path.abspath(file)])
+
 def generate_config(path='config.ini'):
     if not os.path.exists(path):
         writer = configparser.ConfigParser()
@@ -44,13 +57,6 @@ def generate_config(path='config.ini'):
     with open(path,'w') as f:
         writer.write(f)
 
-from shutil import copyfile
-
-
-def copy():
-    copyfile(DEVELOPMENT, RELEASE)
-
-
 def build(which):
     """Creates an OBJ from an LNI map"""
     return subprocess.Popen([p['paths']['w2l'],'obj',os.path.abspath(which)]).wait()
@@ -60,12 +66,12 @@ def commit(which):
     return subprocess.Popen([p['paths']['w2l'],'lni',os.path.abspath(which)]).wait()
 
 def optimize(which):
-    """Create an SLK from an OBJ"""
+    """Create an SLK map from an OBJ or LNI map"""
     return subprocess.Popen([p['paths']['w2l'],'slk',os.path.abspath(which)]).wait()
 
 
-import shutil
 def push(dirs = ['table', 'trigger', 'w3x2lni']):
+    """Copies all specified directories from the development folder to the release folder."""
     release= 'release/'
     development = 'development/'
     for directory in dirs:
@@ -73,9 +79,11 @@ def push(dirs = ['table', 'trigger', 'w3x2lni']):
         shutil.copytree(development+directory, release+directory)
 
 def push_all():
+    """As push, but includes the map directory, which contains many binary files."""
     push(['map', 'table', 'trigger', 'w3x2lni'])
 
 def pull(dirs = ['map','table', 'trigger', 'w3x2lni']):
+    """Copies all specified directories from the release folder to the developlment folder."""
     release= 'release/'
     development = 'development/'
 
@@ -86,33 +94,26 @@ def pull(dirs = ['map','table', 'trigger', 'w3x2lni']):
         if os.path.exists(development+directory):
             shutil.rmtree(development+directory)
         shutil.copytree(release+directory,development+directory)
-    shutil.copy(release+'.w3x',development+'.w3x')
-
-    pass
-    # if path does not exist => create the folder
-    # copytree for each one of these: map, table, trigger, w3xlni
 
 def test_full(build=DEVELOPMENT):
+    """Optimizes an OBJ map and then opens the generated SLK in WC3."""
     optimize(build)
     test('slk',build[:build.rfind('.w3x')])
-    
-import webbrowser
 
-def show_gui(file):
+def open_in_explorer(file):
+    """Opens a file in the Windows explorer (or equivalent)."""
     webbrowser.open(os.path.dirname(p['paths'][file]))
 
-
-import types
-# __doc__ = '\n================\n'.join([(f).__doc__ for f in globals().values() if type(f) == types.FunctionType])
-
 def helpme():
-    for f in [(f) for f in globals().values() if type(f) == types.FunctionType]:
+    """Prints all available user-defined functions and their doc strings"""
+    for f in builderlib.get_user_functions(globals()):
         help(f)
         print("==============================")
 
 
 def ls():
-    for f in [(f) for f in globals().values() if type(f) == types.FunctionType]:
+    """Prints all avaiable user-defined functions."""
+    for f in builderlib.get_user_functions(globals()):
         print (str(f).split()[1])
 
 print('For help type ls()')
