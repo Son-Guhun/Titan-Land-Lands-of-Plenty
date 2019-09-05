1512ddd06181e6af0345bccda53c94ab981e5be6
Son-Guhun <22954418+Son-Guhun@users.noreply.github.com>
2019-09-05 07:21:19 -0300

    Refactored Race Selector, hid Rally icon and fixed hotkeys

--- builderlib/configurehotkeys.py
+++ builderlib/configurehotkeys.py
@@ -6,21 +6,25 @@ HOTKEYS = ['Q','W','E','R',
 for i,hotkey in enumerate(HOTKEYS):
     HOTKEYS[i] = '"{}"'.format(hotkey)
 
+def set_hotkeys(unit_data, unit_list):
+    for i,unit in enumerate(unit_list):
+        #if i > 6:
+        #    i += 1  # Skip rally point ability
+        try:
+            unit = unit_data[unit]
+            unit['Buttonpos_2'] = str(i//4)
+            unit['Buttonpos_1'] = str(i%4)
+            unit['Hotkey'] = HOTKEYS[i]
+        except KeyError:
+            print('Could not find data for trained unit: ' + unit)
+            # print(building + ":", unit_data[building]["Trains"])
+
+
 def configure_trained_units(unit_data, building_list):
     for building in building_list:
         try:
             trained_list = unit_data[building]['Trains'][1:-1].split(',')
-            for i,unit in enumerate(trained_list):
-                if i > 6:
-                    i += 1  # Skip rally point ability
-                try:
-                    unit = unit_data[unit]
-                    unit['Buttonpos_2'] = str(i//4)
-                    unit['Buttonpos_1'] = str(i%4)
-                    unit['Hotkey'] = HOTKEYS[i]
-                except KeyError:
-                    print('Could not find data for trained unit: ' + unit)
-                    print(building + ":", unit_data[building]["Trains"])
+            set_hotkeys(unit_data, trained_list)
         except KeyError:
             print('Could not find any units trained by: ' + building)
         except Exception as e:
@@ -28,16 +32,25 @@ def configure_trained_units(unit_data, building_list):
             print(unit_data[building]["Trains"][1:-1].split(','))
             print(e)
 
-def do_everything(file_path, building_list):
-    f = open(file_path)
-    unit_data = load_unit_data(f)
+def configure_selectors(unit_data, selector_list):
+    for selector in selector_list:
+        upgrade_list = unit_data[selector]['Upgrade'][1:-1].split(',')
+        upgrade_list.remove('e001')
+        trained_list = unit_data[selector]['Trains'][1:-1].split(',')
+        set_hotkeys(unit_data, upgrade_list + trained_list)
+        
+        
     
-    configure_trained_units(unit_data, building_list)
 
-    f.close()
-    f = open(file_path, 'w')
-    unit_data.write(f)
-    f.close()
+def do_everything(file_path, buildings=[], selectors=[]):
+    with open(file_path) as f:
+        unit_data = load_unit_data(f)
+        
+    configure_trained_units(unit_data, buildings)
+    configure_selectors(unit_data, selectors)
+
+    with open(file_path, 'w') as f:
+        unit_data.write(f)
 
 
 file_path = '../development/table/unit.ini'
@@ -56,4 +69,15 @@ buildings = [
     'h0RJ', # Lordaeron
     'h02Y', # Nihonjin
     'h09J', # Nihonjin Magic
+    ]
+
+selectors = [
+    'e005', # Corrupted
+    'e039', # Corrupted (Demons)
+    'e037', # Corrupted (Undead)
+    'e004', # Creeps
+    'e00V', # Elves
+    'e002', # Humans and Dwarves
+    'e00F', # Naga and Murlocs
+    'e003', # Orcs, Ogres, Trolls, Tauren
     ]
