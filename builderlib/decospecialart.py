fb4b44441766cedab1cf8f3831ae27aee161c2f4
Son-Guhun <22954418+Son-Guhun@users.noreply.github.com>
2019-09-17 19:01:42 -0300

    Fixed elevation values for decorations
    - Also fixed a few values for The Dragon Hawk hero

--- builderlib/decospecialart.py
+++ builderlib/decospecialart.py
@@ -3,10 +3,13 @@ from myconfigparser import MyConfigParser, load_unit_data, get_decorations
 
 def configure_decorations(unit_data, decoration_list):
     for decoration in decoration_list:
+        data = unit_data[decoration]
         try:
-            unit_data[decoration]['Specialart'] = '"{}"'.format(',' + unit_data[decoration]['file'][1:-1])
+            data['Specialart'] = '"{}"'.format(',' + data['file'][1:-1])
         except:
             print("Could not find model data for {}.".format(decoration))
+        data['elevPts'] = '0'
+        data['elevRad'] = '0.00'
 
 def do_everything(file_path):
     f = open(file_path)
