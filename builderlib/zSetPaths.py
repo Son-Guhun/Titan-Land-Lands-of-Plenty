6dd04305ff4f362815d971c367bc8a7b0799b669
Son-Guhun <22954418+Son-Guhun@users.noreply.github.com>
2020-01-29 11:19:11 -0300

    Added script that was used to set model/icon paths for reforged

---
+++ builderlib/zSetPaths.py
@@ -0,0 +1,49 @@
+import os
+
+rootdir = '../release/resource'
+all_files = set()
+
+for subdir, dirs, files in os.walk(rootdir):
+    res = 'resource'
+    subdir = subdir[subdir.find(res)+len(res):]
+    if subdir != '':
+        subdir = subdir[1:] + os.sep
+
+    for file in files:
+        filepath = subdir + file
+
+        all_files.add(filepath.lower())
+
+from myconfigparser import MyConfigParser, load_unit_data, Section
+import s2id
+
+dataBase = "..\\development\\table\\unit.ini"
+
+# print('{}: {}'.format(rawcode, unit_data[rawcode]['Name']))
+
+def do(dataBase, upgrade_lists):
+    
+    with open(dataBase) as f:
+        data = load_unit_data(f)
+
+    for unit in (Section(data[u]) for u in data):
+        model = unit['file'].replace('\\\\', '\\')[1:-1]
+        icon = unit['Art'].replace('\\\\', '\\')[1:-1]
+
+        if model.endswith('.mdl'):
+            model = model[:model.rfind('.')] + '.mdx'
+        
+        try:
+            if model.lower() not in all_files:
+                # print('[{}]'.format(unit._section.name), unit['Name'])
+                unit._section['file'] = '"{}"'.format('war3.w3mod:'+model).replace('\\','\\\\')
+            if icon.lower() not in all_files:
+                unit._section['Art'] = '"{}"'.format('war3.w3mod:'+icon).replace('\\','\\\\')
+                # print(icon)
+        except Exception as e:
+            print(unit._section.name)
+            raise e
+
+    with open(dataBase, 'w') as f:
+        data.write(f)
+
