0c8497695b804c41d82f454f96f1c64c49349f60
Son Guhun <22954418+Son-Guhun@users.noreply.github.com>
2020-02-01 23:47:52 -0300

    Moved controller logic out of main objecteditor script

--- _scripts/objecteditor.py
+++ _scripts/objecteditor.py
@@ -3,34 +3,11 @@ import PySimpleGUI as sg
 from myconfigparser import load_unit_data, Section
 
 
-from objecteditor.view import newselector
-from objecteditor.model.search import map_substrings
+from objecteditor.controller.newselector import open_window
 
 dataBase = '../development/table/unit.ini'
 
 with open(dataBase) as f:
     data = load_unit_data(f)
 
-options = []
-for u in data:
-    unit = Section(data[u])
-    if '[sele]' in unit['EditorSuffix'] or u =='e001':
-        options.append('{name} [{code}]'.format(code=u, name=unit['Name'][1:-1]))
-        
-strings = map_substrings(options)
-
-
-window = sg.Window('Everything bagel', newselector.layout, default_element_size=(40, 1), grab_anywhere=False).Finalize()     
-window.find_element('Options').Update(options)
-
-while True:
-    event, values = window.read()
-
-    search = values['Search'].lower()
-    if search in strings:
-        window.find_element('Options').Update(strings[search])
-    else:
-        window.find_element('Options').Update(options)
-
-    if event is None:
-        break
+open_window(data)
---
+++ _scripts/objecteditor/controller/newselector.py
@@ -0,0 +1,33 @@
+import PySimpleGUI as sg
+from ..model.objectdata import ObjectData
+from ..model.search import map_substrings
+from ..view import newselector
+
+from myconfigparser import Section
+
+def open_window(data):
+    options = []
+    for u in data:
+        unit = Section(data[u])
+        if '[sele]' in unit['EditorSuffix'] or u =='e001':
+            options.append('{name} [{code}]'.format(code=u, name=unit['Name'][1:-1]))
+            
+    strings = map_substrings(options)
+
+
+    window = sg.Window('Everything bagel', newselector.layout, default_element_size=(40, 1), grab_anywhere=False).Finalize()     
+    window.find_element('Options').Update(options)
+
+    while True:
+        event, values = window.read()
+
+        search = values['Search'].lower()
+        if search in strings:
+            window.find_element('Options').Update(strings[search])
+        else:
+            window.find_element('Options').Update(options)
+
+        if event is None:
+            break
+        elif event == 'Submit':
+            sg.popup('Success')
