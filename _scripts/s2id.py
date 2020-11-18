aa8cb95d858583f037971c84e5f7143ada9dc8bc
Son Guhun <22954418+Son-Guhun@users.noreply.github.com>
2020-11-18 14:53:01 -0300

    Rawcode class is now safe by default
    - Also added to_int() method and match(other) method.
    - Rawcodes are now stored in a WeakValueDictionary so that they can be reused, like normal strings.

--- _scripts/s2id.py
+++ _scripts/s2id.py
@@ -1,10 +1,16 @@
-"""Used to work with FourCC object codes used by Warcraft III.
+"""
+Used to work with FourCC's (four character codes) used by Warcraft III to identify objects.
 
 Provides a Rawcode class which inherits from str and can be added with integers.
-Example: Rawcode('H009') + 2 => Rawcode('H00B')
+Examples: 
+    - Rawcode('H009') + 2           => Rawcode('H00B')    # Considers only alphanumeric rawcodes.
+    - Rawcode('hfoo').to_int()      => 130701168          # Returns in-game value of rawcode.
+    - Rawcode('Hart').match('abAB') => Rawcode('haRT')    # Matches capitalization of another rawcode.
+    - Rawcode('Hart').match('0000') => Rawcode('HART')    # Numbers are considered uppercase.
 """
-alphabet = 'abcdefghijklmnpqrstuvwxyz'
-dictionary = {k:v for (k,v) in zip(alphabet,range(len(alphabet)))}
+import string
+
+alphabet = '0123456789' + string.ascii_lowercase
 
 def base36encode(number, alphabet='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
     """Converts an integer to a base36 string."""
@@ -30,28 +36,57 @@ def base36decode(number):
 
 RAWCODE_MAX = int('ZZZZ', 36) + 1
 
+
+from weakref import WeakValueDictionary
+
 class Rawcode(str):
+
+    _objects = WeakValueDictionary()
+    _chars = set(alphabet + alphabet.upper())
+
+    def __new__(cls, object):
+        r = super().__new__(cls, object)
+        chars = cls._chars
+
+        if r in cls._objects:
+            return cls._objects[r]
+        
+        if len(r) != 4:
+            raise ValueError("Rawcode string must be 4 characters long.")
+
+        if not(r[0] in chars and r[1] in chars and r[2] in chars and r[3] in chars):
+            raise ValueError("Rawcode string must be alphanumeric.")
+
+        cls._objects[r] = r
+
+        return r
     
     def __add__(self, n):
         return Rawcode(base36encode(base36decode(self)+n))
 
     def __repr__(self):
         return "Rawcode('{}')".format(self)
-        
 
-class RawcodeSafe(Rawcode):
-    
-    def __new__(cls, object):
-        r = super().__new__(cls, object)
-        if len(r) != 4:
-            raise ValueError("Rawcode string must be 4 characters long.")
-        for char in r:
-            if 0 <= ord(char) <= 31 or ord(char) >= 127:
-                raise ValueError("Rawcode string must be ASCII.")
-        return r
+    def to_int(self):
+        return ord(self[0]) * ord(self[1]) * ord(self[2]) * ord(self[3])
+
+    def match(self, other):
+        r = ''.join([
+            self[0].lower() if other[0].islower() else self[0].upper(),
+            self[1].lower() if other[1].islower() else self[1].upper(),
+            self[2].lower() if other[2].islower() else self[2].upper(),
+            self[3].lower() if other[3].islower() else self[3].upper()
+        ])
+
+        return self if self == r else type(self)(r)
+
+
+# Declare RawcodeSafe class for compatability
+RawcodeSafe = Rawcode
+
 
 def rawcode_range(init, size):
-    """Example: rawcode_range('H000',3) => yields 'H000','H001','H002'"""
+    """Example: rawcode_range('H000',3) => yields 'H000','H001','H002' as Rawcodes"""
     if type('init') == str:
         init = Rawcode(init)
     for i in range(size):
